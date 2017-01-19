9a8b612aa7c22d0fa564ffb7a7b3c41d82f7cf43
wenchichou <wenchichou@users.noreply.github.com>
2017-01-18 23:46:03 -0500

    Update GWAS enrichment.py

--- GWAS enrichment.py
+++ GWAS enrichment.py
@@ -1,39 +1,16 @@
+# Try to finish the first 3 steps by Thursday Taiwan Time.
+##1.1  import eQTL bags in python panda dataframe
+
+##1.2  set the first column as each SNPs positions
+
+##1.3  set the second column as the bag index
 
 
 
 
+# Try to finish the last 3 steps by Friday Taiwan Time.
+##2.1 import GWAS file
 
+##2.2 map each GWAS SNPs with the first column of the dataframe
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## import eQTL bags in python panda dataframe
-
-## set the first column as each SNPs positions
-
-## set the second column as the bag index
-
-
-## import GWAS file
-
-## map each GWAS SNPs with the first column of the dataframe
-
-## if mapped, put into each bag list, else drop it
+##2.3 if mapped, put into each bag list, else drop it
